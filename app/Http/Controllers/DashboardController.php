b327ee900a291ad8ffcfb14e39953311f58afab8
godwillcodes <iamgodwillb@gmail.com>
2023-09-15 02:08:07 +0300

    small admin section, assuming we need analytics
    Not in the sheet, but I was having too much fun

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'authors' => Author::count(),
+            'books' => Book::count(),
+            'users' => User::count(),
+        ];
+
+        return inertia('Dashboard', ['data' => $data]);
+    }
+}
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,27 +1,28 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { Head } from '@inertiajs/react'
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({ auth, data }) {
   return (
     <AuthenticatedLayout user={auth.user} header={<h2 className="dark:text-gray-200 text-gray-800 text-xl font-semibold leading-tight">Karibu Shelfie ❤️</h2>}>
       <Head title="Dashboard" />
 
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div class="sm:rounded-lg">
-            <dl class="grid gap-5 grid-cols-1 mt-5 sm:grid-cols-3">
-              <div class="px-4 py-5 bg-orange-500 rounded-lg overflow-hidden sm:p-6">
-                <dt class="text-white text-sm font-medium truncate">Total Authors</dt>
-                <dd class="mt-1 text-gray-900 text-3xl font-semibold tracking-tight">71,897</dd>
+          <div className="sm:rounded-lg">
+            <dl className="grid gap-5 grid-cols-1 mt-5 sm:grid-cols-3">
+              <div className="px-4 py-5 bg-gray-800 rounded-lg overflow-hidden sm:p-6">
+                <dt className="text-white text-lg font-medium truncate">Total Authors</dt>
+                <dd className="mt-1 text-orange-400 text-3xl font-semibold tracking-tight">{data.authors}</dd>
               </div>
-              <div class="px-4 py-5 bg-orange-500 rounded-lg overflow-hidden sm:p-6">
-                <dt class="text-white text-sm font-medium truncate">Total Books</dt>
-                <dd class="mt-1 text-gray-900 text-3xl font-semibold tracking-tight">71,897</dd>
+              <div className="px-4 py-5 bg-gray-800 rounded-lg overflow-hidden sm:p-6">
+                <dt className="text-white text-lg font-medium truncate">Total Books</dt>
+                <dd className="mt-1 text-orange-400 text-3xl font-semibold tracking-tight">{data.books}</dd>
               </div>
-              <div class="px-4 py-5 bg-orange-500 rounded-lg overflow-hidden sm:p-6">
-                <dt class="text-white text-sm font-medium truncate">Current Users</dt>
-                <dd class="mt-1 text-gray-900 text-3xl font-semibold tracking-tight">71,897</dd>
+              <div className="px-4 py-5 bg-gray-800 rounded-lg overflow-hidden sm:p-6">
+                <dt className="text-white text-lg font-medium truncate">Current Users</dt>
+                <dd className="mt-1 text-orange-400 text-3xl font-semibold tracking-tight">{data.users}</dd>
               </div>
+             
             </dl>
           </div>
         </div>
